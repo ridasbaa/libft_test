9422c0f3c1a2faaf6c4f16542a1ec7b84de9a62c
Rida Sbaa <rsbaa@c3r7p4.1337.ma>
2025-10-30 04:23:46 +0100

    new changes

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: rsbaa <rsbaa@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 01:16:02 by rsbaa             #+#    #+#             */
-/*   Updated: 2025/10/30 03:47:33 by rsbaa            ###   ########.fr       */
+/*   Updated: 2025/10/30 04:23:41 by rsbaa            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	void			*area;
 
 	if (size != 0 && nmemb > SIZE_MAX / size)
-	area = malloc(nmemb * size);
+		area = malloc(nmemb * size);
 	if (!area)
 		return (NULL);
 	ft_bzero(area, nmemb * size);
